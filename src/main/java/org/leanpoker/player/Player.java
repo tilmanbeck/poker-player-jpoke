91eda3aa402649c5ac1131ccde2bda42c6384e30
Mathis Neumann <mathis@simpletechs.net>
2016-05-14 10:20:23 +0200

    random player

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,14 +2,18 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
+import java.util.Random;
 
 public class Player {
 
+    static Random rand = new Random();
+    static int min = 0;
+    static int max = 100;
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+
+        return rand.nextInt((max - min) + 1) + min;
     }
 
     public static void showdown(JsonElement game) {
