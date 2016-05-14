50098cfd821940a5ec8015797c4ce53e05617486
devnull <tilmanbeck90@gmail.com>
2016-05-14 10:42:33 +0200

    - first parse

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Random;
 
@@ -12,6 +14,22 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        JsonObject json = request.getAsJsonObject();
+
+        String tournamentId = json.get("tournament_id").getAsString();
+        String game_id = json.get("game_id").getAsString();
+        int minimumRaise = json.get("minimum_raise").getAsInt();
+        int current_buy_in = json.get("current_buy_in").getAsInt();
+        int round = json.get("round").getAsInt();
+        int bet_index = json.get("bet_index").getAsInt();
+        int pot = json.get("pot").getAsInt();
+        int dealer = json.get("dealer").getAsInt();
+        int orbits = json.get("orbits").getAsInt();
+        int in_action = json.get("in_action").getAsInt();
+        JsonObject players = json.getAsJsonObject("players");
+        JsonObject community_cards = (JsonObject) json.get("community_cards");
+
+
 
         return rand.nextInt((max - min) + 1) + min;
     }
