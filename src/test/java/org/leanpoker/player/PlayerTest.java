fba392d6c7aca17f36df8c931f5b45bbf73809f1
dima <drgn13@mail.ru>
2016-05-14 14:23:54 +0200

    Alpha-version of PlayerTest

--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,19 +1,26 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.leanpoker.player.Player;
 import org.junit.Test;
+import java.io.FileReader;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class PlayerTest {
 
     @Test
     public void testBetRequest() throws Exception {
 
-        JsonElement jsonElement = new JsonParser().parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
+        Player pl = new Player ();
+        Object obj = new JsonParser().parse(new FileReader("src/test/test.json"));
+        JsonObject jsobj = (JsonObject) obj;
+        JsonElement jsonEm = jsobj;
 
-        assertEquals(0, Player.betRequest(jsonElement));
+        assertNotNull("Null was returned", Player.betRequest(jsonEm));
 
     }
 }
